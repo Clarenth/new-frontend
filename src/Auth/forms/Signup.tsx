579b493d67f5611b7f5bd7ebe5586b003dc531b3
Clarenth <standadit.dev@protonmail.com>
2023-11-19 20:56:52 -0500

    Basic auth form

--- src/Auth/forms/Signup.tsx
+++ src/Auth/forms/Signup.tsx
@@ -1,11 +1,83 @@
+// Libraries
+import { useForm } from "react-hook-form"
+import * as zod from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+// Internal Lib
+import { SignupValidation } from "@/lib/validation"
+
+// Components
 import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+// Styles
+
+
 
 const Signup = () => {
+  
+  // 1. Define your form.
+  const form = useForm<zod.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      email: "",
+      password: "",
+      phone_number: "",
+      job_title: "",
+      office_address: "",
+      employee_identity_data:
+      {
+        first_name: "",
+        middle_name: "",
+        last_name:"",
+        sex: "",
+        gender: "",
+        age:"",
+        height:"",
+        home_address:"",
+        birthdate:"",
+        birthplace:"",
+      },
+      security_access_level: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: zod.infer<typeof SignupValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="assets/images/logo.svg" alt="logo" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">To use Colony Office, create an account</p>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </div>
+    </Form>
   )
 }
 
-export default Signup
+export default Signup;
