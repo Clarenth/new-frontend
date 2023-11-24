31d326aab5e3eaf49f947efd7c4b4c2f424e6842
Clarenth <standadit.dev@protonmail.com>
2023-11-24 10:26:54 -0500

    Basic working skeleton of file uploader

--- src/components/forms/DocsCreateForm.tsx
+++ src/components/forms/DocsCreateForm.tsx
@@ -17,18 +17,35 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Textarea } from "../ui/textarea"
+import FileUploader from "../shared/FileUploader"
+import { DropdownMenu } from "../ui/dropdown-menu"
+import SecurityLevelDropdown from "../shared/SecurityLevelDropdown"
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  title: z.string().min(2, {
+    message: "Title must be at least 2 characters.",
   }),
+  description: z.string().min(2, {
+    message: "Description must be at least 2 characters.",
+  }),
+  language: z.string().min(2, {
+    message: "Language must be at least 2 characters.",
+  }),
+  security_access_level: z.string().min(2, {
+    message: "SecurityAccessLevel returns Dropdown error.."
+  })
 })
 
-const DocsCreateForm = () => {
+const DocsCreateForm = ({ doc }) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      title: "",
+      description: "",
+      language: "",
+      security_access_level: "",
+      //files: ,
     },
   })
 
@@ -44,21 +61,82 @@ const DocsCreateForm = () => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
         <FormField
           control={form.control}
-          name="username"
+          name="title"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel className="shad-form_label">Title</FormLabel>
               <FormControl>
                 <Input placeholder="shadcn" {...field} />
               </FormControl>
               <FormDescription>
-                This is your public display name.
+                Title of the Document.
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Description</FormLabel>
+              <FormControl>
+                <Textarea placeholder="shadcn" className="shad-textarea custom-scrollbar" {...field} />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="language"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Language</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                Language of the Document.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="files"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Upload Files</FormLabel>
+              <FormControl>
+                <FileUploader 
+                  fieldChange={field.onChange}
+                  mediaURL={ doc?.fileURLW }
+                />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="security_access_level"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Security Access Level</FormLabel>
+              <FormControl>
+                <SecurityLevelDropdown />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex gap-4 items-center justify-end">
+          <Button type="button" className="shad-button_dark_4">Cancel</Button>
+          <Button type="submit" className="shad-button_primary whitespace-nowrap">Submit</Button>
+        </div>
       </form>
     </Form>
   )
